597300a651a3d2072bda87acaa9143a1197c84f9
Carter Schuerman <carter.schuerman@trojans.dsu.edu>
2025-03-31 17:49:22 -0500

    Add files via upload

---
+++ TrafficJam/carGame/sim.py
@@ -0,0 +1,106 @@
+import pygame
+import time
+
+from models import Vehicle
+import random
+from utils import load_sprite
+from traffic_light import TrafficLight
+
+
+class Simulation:
+
+    def __init__(self):
+        self.__init_pygame()
+        self.screen = pygame.display.set_mode((800, 800))
+        self.background = load_sprite("intersection", False)
+        self.clock = pygame.time.Clock()
+        #self.vehicle = Vehicle()
+        self.maxCars = 32
+        self.vehicles = []
+        self.carCount = 0
+        self.turn = 0
+        self.crossing = [
+            #start pos       stop line       goal           end            Direction
+            [(0, 435),    (225, 435), (575, 435),    (800, 435),   (1, 0)],  #road 1
+            [(0, 470),    (225, 470), (257.5, 225),    (257.5, 800),   (1, 0)],  #road 2
+            [(435, 800),  (457.5, 575), (457.5, 225),    (457.5, 0),     (0, -1)], #road 3
+            [(470, 800),  (532.5, 575), (575, 532.5),    (800, 532.5),   (0, -1)], #road 4
+            [(800, 330),  (575, 457.5), (225, 457.5),    (0, 457.5),     (-1, 0)], #road 5
+            [(800, 360),  (575, 532.5), (532.5, 225),    (532.5, 0),     (-1, 0)], #road 6
+            [(330, 0),    (332.5, 225), (332.5, 575),    (332.5, 800),   (0, 1)],  #road 7
+            [(370, 0),    (257.5, 225), (225, 257.5),    (0, 257.5),     (0, 1)],  #road 8
+        ]
+
+        self.spawn_vehicles()
+        self.traffic_lights = [
+            TrafficLight((305, 435), "green"),
+            TrafficLight((305, 470), "green"),
+            TrafficLight((490, 330), "green"), 
+            TrafficLight((490, 360), "green"),   
+            TrafficLight((360, 305), "green"),
+            TrafficLight((330, 305), "green"),
+            TrafficLight((435, 490), "red"),
+            TrafficLight((470, 490), "red") 
+        ]
+
+        generator = pygame.event.Event(1)
+
+        pygame.time.set_timer(generator, 550)
+
+    def __init_pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Traffic Jam")
+
+    def main_loop(self):
+        while True:
+            self._handle_input()
+            self._process_game_logic()
+            self.draw()
+            dt = self.clock.tick() / 100
+
+    def _handle_input(self):
+        for event in pygame.event.get():
+            if event.type == 1:
+                self.spawn_vehicles()
+            if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
+                quit()
+
+    def _process_game_logic(self):
+        dt = self.clock.tick() / 1000  # Convert to seconds
+        for traffic_light in self.traffic_lights:
+            traffic_light.update(dt)
+        for vehicle in self.vehicles:
+            vehicle.update(self.vehicles, self.traffic_lights)
+            if vehicle.position.x < 0 or vehicle.position.x > 1200 or vehicle.position.y < 0 or vehicle.position.y > 800:
+                self.vehicles.pop(self.vehicles.index(vehicle))
+                self.carCount -= 1
+    
+    def draw(self):
+        self.screen.blit(self.background, (0, 0))
+        for traffic_light in self.traffic_lights:
+            traffic_light.draw(self.screen)
+        for vehicle in self.vehicles:
+            vehicle.draw(self.screen)
+        pygame.display.flip()
+
+    def spawn_vehicles(self):
+        goal = random.randint(1, 3)
+        count = self.carCount
+        roadI = 0
+        for road in self.crossing:
+            if self.carCount + 1 >= self.maxCars:
+                return
+            startPoint = road[0]
+            direction = road[4]
+            vehicle = Vehicle(startPoint, direction, goal, road, roadI)
+            self.vehicles.append(vehicle)
+            self.carCount += 1
+            roadI += 1
+
+            
+
+# pygame setup
+
+
---
+++ TrafficJam/carGame/traffic_light.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.math import Vector2
+
+class TrafficLight:
+    def __init__(self, position, state="red", green_duration=5, yellow_duration=2, red_duration=5):
+        self.position = Vector2(position)
+        self.green_duration = green_duration
+        self.yellow_duration = yellow_duration
+        self.red_duration = red_duration
+        self.state = state # Initial state
+        self.timer = 0.0
+
+    def update(self, dt):
+        self.timer += dt
+        if self.state == "red" and self.timer >= self.red_duration:
+            self.state = "green"
+            self.timer = 0
+        elif self.state == "green" and self.timer >= self.green_duration:
+            self.state = "yellow"
+            self.timer = 0
+        elif self.state == "yellow" and self.timer >= self.yellow_duration:
+            self.state = "red"
+            self.timer = 0
+
+    def draw(self, surface):
+        color = (255, 0, 0) if self.state == "red" else (255, 255, 0) if self.state == "yellow" else (0, 255, 0)
+        pygame.draw.circle(surface, color, (int(self.position.x), int(self.position.y)), 10)
